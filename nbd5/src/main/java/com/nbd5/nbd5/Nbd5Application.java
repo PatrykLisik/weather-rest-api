c4b4b634f32d96668620f295223eaeb035001d17
Patryk Piglowski <195682@edu.p.lodz.pl>
2019-04-02 10:39:51 +0000

    Initialize starter project with Spring Boot and Cassandra dependency

---
+++ nbd5/src/main/java/com/nbd5/nbd5/Nbd5Application.java
@@ -0,0 +1,13 @@
+package com.nbd5.nbd5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nbd5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nbd5Application.class, args);
+	}
+
+}
