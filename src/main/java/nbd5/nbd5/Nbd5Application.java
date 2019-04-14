99c3691b03e42db1b79e9215e52709f06aaef551
Patryk Lisik <210254@edu.p.lodz.pl>
2019-04-14 16:41:43 +0200

    Working spring project

--- src/main/java/nbd5/nbd5/Nbd5Application.java
+++ src/main/java/nbd5/nbd5/Nbd5Application.java
@@ -1,4 +1,4 @@
-package com.nbd5.nbd5;
+package nbd5.nbd5;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/nbd5/nbd5/Nbd5ApplicationTests.java
+++ src/test/java/nbd5/nbd5/Nbd5ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.nbd5.nbd5;
+package nbd5.nbd5;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
